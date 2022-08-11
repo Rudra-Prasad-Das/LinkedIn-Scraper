9c4622db8ea5d4740b6e785b9ae10b168935a250
Rudra Prasad Das <83531557+Rudra-Prasad-Das@users.noreply.github.com>
2022-08-11 16:33:44 +0530

    added contact-info

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ element_ID.send_keys(username)
 element_ID=browser.find_element(By.ID,"password")
 element_ID.send_keys(password)
 element_ID.submit()
-visitingProfile='/in/<profile-link>/'
+visitingProfile='/in/<profile-name>/'
 fullLink='https://www.linkedin.com'+visitingProfile
 browser.get(fullLink)
 src=browser.page_source
@@ -29,8 +29,19 @@ print(name)
 loc_div=soup.find('span',{'class':'text-body-small'}).get_text().strip()
 # name_loc=loc_div.find_all('span')
 print(loc_div)
+contact_link='overlay/contact-info/'
+profileLink='https://www.linkedin.com'+visitingProfile+contact_link
+browser.get(profileLink)
+src=browser.page_source
+soup=BeautifulSoup(src,'lxml')
+contact_dict=[]
+contact_details=soup.find_all('a',{'class':'pv-contact-info__contact-link'})
+for i in range(len(contact_details)):
+    contact_dict.append(contact_details[i].get_text().strip())
+# print(contact_details[0].get_text().strip())
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["LinkedIn-db"]
+mydb = myclient["LinkedInContact-db"]
 mycol = mydb["users"]
-mydict = { "name": name, "location":loc_div }
+mydict = { "name": name, "location":loc_div,"contact-info":contact_dict}
 mycol.insert_one(mydict)
+
